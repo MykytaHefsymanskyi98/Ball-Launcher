2ff85aad79b0827fcc40f01f1a806d4f3d1a677b
MykytaHefsymanskyi98 <mykytahefsymanskyi@gmail.com>
2021-09-07 15:57:19 +0300

    Implemented level navigation through UI

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] GameObject levelFinishedCanvas;
+
+    float timeScaleStarting = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelFinishedCanvas.SetActive(false);
+        Time.timeScale = timeScaleStarting;
+    }
+
+    public void StarHitted()
+    {
+        Time.timeScale = 0f;
+        levelFinishedCanvas.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/Sceneloader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneloader : MonoBehaviour
+{
+    int currentSceneIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameCompletedScene()
+    {
+        SceneManager.LoadScene("Game Completed");
+    }
+    
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadThisLevel()
+    {
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
